c2037b19106c4e009772dbb2f7213057ec53a425
ImManiKanta <chinthamanikanta1@gmail.com>
2026-01-31 16:16:37 +0530

    shell roboshop

---
+++ 4.redis.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+dnf module disable redis -y
+
+dnf module enable redis:7 -y
+
+dnf install redis -y 
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
+
+systemctl enable redis 
+
+systemctl start redis 
