1765826f56728e1ef72faddb53840231bbd2dbdf
ImManiKanta <chinthamanikanta1@gmail.com>
2026-01-31 11:34:59 +0530

    ec2,route53 shell resource provision

---
+++ roboshop_resource_provision.sh
@@ -0,0 +1,63 @@
+#Resource provision for roboshop proj using shell
+
+SG_ID="sg-07d17be865ead02f3" # replace with your ID
+AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z00114331R4EBNTWP3VT8"
+DOMAIN_NAME="manidevops.online"
+
+for instance in $@
+do 
+INSTANCE_ID=$( aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type "t3.micro" \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text )
+
+    if [ $instance == "frontend" ]; then
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$DOMAIN_NAME" # manidevops.online
+    else
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.manidevops.online
+    fi
+
+    echo "IP Address: $IP"
+
+ aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record",
+        "Changes": [
+            {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "'$RECORD_NAME'",
+                "Type": "A",
+                "TTL": 1,
+                "ResourceRecords": [
+                {
+                    "Value": "'$IP'"
+                }
+                ]
+            }
+            }
+        ]
+    }
+    '
+
+    echo "record updated for $instance"
+
+done
