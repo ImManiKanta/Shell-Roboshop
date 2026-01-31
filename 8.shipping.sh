85c787a81b79e40bff8b38112b87371f401e18f5
ImManiKanta <chinthamanikanta1@gmail.com>
2026-01-31 17:45:11 +0530

    shell roboshop

--- 8.shipping.sh
+++ 8.shipping.sh
@@ -51,8 +51,8 @@ dnf install mysql -y
 
 mysql -h mysql.manidevops.online -uroot -pRoboShop@1 < /app/db/schema.sql
 
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/app-user.sql 
+mysql -h mysql.manidevops.online -uroot -pRoboShop@1 < /app/db/app-user.sql 
 
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/master-data.sql
+mysql -h mysql.manidevops.online -uroot -pRoboShop@1 < /app/db/master-data.sql
 
 systemctl restart shipping
